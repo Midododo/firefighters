04d9f4728c4127909d29580d17ebe57a0841dbc5
MukaiNaohito <dosukoi_0222@ezweb.ne.jp>
2018-11-14 12:34:35 +0900

    // うんち ver 2.0

---
+++ Assets/Mukai/Scripts/UI/SampleNumber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleNumber : MonoBehaviour
+{
+
+    NumberImageRenderer numberImageRenderer = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        numberImageRenderer = GetComponent<NumberImageRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 浮動小数点
+        numberImageRenderer.Render(100.0);
+
+        // 整数
+        //numberImageRenderer.Render(100);
+    }
+}
