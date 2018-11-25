5591485308c06c0831cf094d0fc41ecdd8c65ba6
MukaiNaohito <dosukoi_0222@ezweb.ne.jp>
2018-11-26 00:02:07 +0900

    // ゲーム画面

---
+++ Assets/Mukai/Scripts/UI/Timer/TimeCountSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeCountSystem : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 60;
+    [SerializeField]
+    private bool flag = false;
+
+    //public ballScript BallScript;
+
+    void Start()
+    {
+        //初期値60を表示
+        //float型からint型へCastし、String型に変換して表示
+        GetComponent<Text>().text = ((int)time).ToString();
+    }
+
+    void Update()
+    {
+        if (flag)
+        {
+            //1秒に1ずつ減らしていく
+            time -= Time.deltaTime;
+            GetComponent<Text>().text = ((int)time).ToString();
+            //0行ったら終了
+            if (time == 0) flag = false;
+        }
+    }
+}
+
--- Assets/Mukai/Scripts/UI/Title/PushLogoController.cs
+++ Assets/Mukai/Scripts/UI/Title/PushLogoController.cs
@@ -40,8 +40,8 @@ public class PushLogoController : MonoBehaviour
             // 点滅アニメーションが終了したら
             else if (gameObject.GetComponent<Blinker>().Count == gameObject.GetComponent<Blinker>().cnt)
             {
-                GameObject.Find("Fade").GetComponent<Fade>().SetFadeOutFlag(next);
-                if (!GameObject.Find("Fade").GetComponent<Fade>().IsFading())
+                GameObject.Find("Panel").GetComponent<Fade>().SetFadeOutFlag(next);
+                if (!GameObject.Find("Panel").GetComponent<Fade>().IsFading())
                 {
                     SoundManager.Instance.Stop(AudioKey.TitleBGM);
                 }
@@ -51,7 +51,7 @@ public class PushLogoController : MonoBehaviour
 
         else if (!fade)
         {        // フェードインが完了したら
-            if (!GameObject.Find("Fade").GetComponent<Fade>().IsFading())
+            if (!GameObject.Find("Panel").GetComponent<Fade>().IsFading())
             {
                 SoundManager.Instance.Play(AudioKey.TitleBGM);
                 this.gameObject.GetComponent<Image>().color = new Color(255.0f, 255.0f, 255.0f, 1.0f);
