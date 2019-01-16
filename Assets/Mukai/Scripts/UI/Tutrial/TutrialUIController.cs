2d923349f3b1e8ebfc16b4e50ff1663d8d637f89
MukaiNaohito <dosukoi_0222@ezweb.ne.jp>
2019-01-16 18:50:12 +0900

    ・　ごめんさっきのばかしてましたこっちが最新です

--- Assets/Mukai/Scripts/UI/Tutrial/TutrialUIController.cs
+++ Assets/Mukai/Scripts/UI/Tutrial/TutrialUIController.cs
@@ -17,19 +17,19 @@ public class TutrialUIController : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        // テスト
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Window.ShowWindow();    // 第一メッセージから順に表示(DeleteWindowを一回挟むと次のメッセージが出る)
-        }
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            Window.DeleteWindow();  // 必ずShowWIndowのとワンセット。先にShowWindow
-        }
+        //// テスト
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Window.ShowWindow();    // 第一メッセージから順に表示(DeleteWindowを一回挟むと次のメッセージが出る)
+        //}
+        //if (Input.GetKeyDown(KeyCode.Backspace))
+        //{
+        //    Window.DeleteWindow();  // 必ずShowWIndowのとワンセット。先にShowWindow
+        //}
 
-        // テスト
-        if (Input.GetKeyDown(KeyCode.W)) Type.type_right.ChangeWaterType();
-        if (Input.GetKeyDown(KeyCode.S)) Type.type_left.ChangeWaterType();
+        //// テスト
+        //if (Input.GetKeyDown(KeyCode.W)) Type.type_right.ChangeWaterType();
+        //if (Input.GetKeyDown(KeyCode.S)) Type.type_left.ChangeWaterType();
 
     }
 }
