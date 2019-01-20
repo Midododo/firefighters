e1416b3f9219ee8e6cedf27ccda79e9675227d2f
MukaiNaohito <dosukoi_0222@ezweb.ne.jp>
2019-01-21 02:50:19 +0900

    // 変更点 // GameSound.csとTutrialSound.cs // どちらもCanvasにSEのfire_masterを追加

--- Assets/Mukai/Scripts/Sound/GameSound.cs
+++ Assets/Mukai/Scripts/Sound/GameSound.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class GameSound : MonoBehaviour
 {
 
+    private AudioSource sources;
+
     // Use this for initialization
     void Start()
     {
+        sources = GetComponent<AudioSource>();
+
         AudioManager.Instance.StopBgm();
         AudioManager.Instance.PlayBgm("Game_ON_BGM");
         AudioManager.Instance.Volume.bgm = 0.22f;           // カウントダウンが終了するまでは小さい音路湯で流す
+        sources.Play();
+
     }
 
     // Update is called once per frame
--- Assets/Mukai/Scripts/Sound/TutrialSound.cs
+++ Assets/Mukai/Scripts/Sound/TutrialSound.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class TutrialSound : MonoBehaviour
 {
 
+    private AudioSource sources;
+
     // Use this for initialization
     void Start()
     {
+        sources = GetComponent<AudioSource>();
         AudioManager.Instance.StopBgm();
         AudioManager.Instance.PlayBgm("Game_OFF_BGM");
         AudioManager.Instance.Volume.bgm = 0.7f;
+        sources.Play();
         //SoundManager.Instance.Play(AudioKey.GameBGM_OFF);
     }
 
